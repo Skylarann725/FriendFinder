f402b67e9187cc2a8dc7bfc4def7a07412ffa509
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-05-05 16:04:31 -0400

    pictures not working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friendsArray = [{
     name: "Arnold",
-    photo: "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=47625075",
+    photo: "https://static1.comicvine.com/uploads/original/11121/111213005/5301702-8463448373-81jtN.jpg?width=400",
     scores: ["4", "4", "4", "4", "4", "4", "1", "1", "1", "2"]
 }, {
 	name: "Helga",
-	photo: "https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2F7a7e41b6-505f-467d-a210-75304b39bdda.jpg",
+	photo: "https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2F7a7e41b6-505f-467d-a210-75304b39bdda.jpg?width=400",
 	scores: ["1", "2", "1", "3", "4", "1", "3", "4", "1", "3"]
 }, {
 	name: "Gerald",
-	photo: "https://vignette2.wikia.nocookie.net/heyarnold/images/c/c8/GeraldShow.jpg/revision/latest?cb=20120613120813",
+	photo: "http://www.hey-arnold.com/Arnold/Gerald5.jpg?width=400",
 	scores: ["1", "2", "3", "4", "1", "2", "3", "4", "1", "2"]
 }, {
 	name: "Harold",
-	photo: "https://i.ytimg.com/vi/GJJVx_T0S3c/hqdefault.jpg",
+	photo: "https://i.ytimg.com/vi/GJJVx_T0S3c/hqdefault.jpg?width=400",
 	scores: ["2", "1", "4", "3", "2", "1", "1", "1", "3", "4"]
 }];
 
